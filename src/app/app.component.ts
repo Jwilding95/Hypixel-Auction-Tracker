b2d2ae4e73b430f4304f155c9409933164ac92ed
Jordan <jordan.wilding@trailar.co.uk>
2024-07-01 16:36:49 +0100

    add location stratergy for 404 on refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { FilterComponent } from './components/filter/filter.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MatIconModule } from '@angular/material/icon';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None,
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
 })
 export class AppComponent {
 
