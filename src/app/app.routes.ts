82d7ecd16bcaa3f2edb93e270ae5ee60f1a5c0a3
Jordan <jordan.wilding@trailar.co.uk>
2024-07-06 15:27:56 +0100

    add bazaar page and routing. Change header to accomodate bazaar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AdvancedSearchComponent } from './components/advanced-search/advanced-search.component';
+import { BazaarComponent } from './bazaar/bazaar.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'advanced-search', component: AdvancedSearchComponent },
+    { path: 'bazaar', component: BazaarComponent },
     { path: '**', redirectTo: '/home', pathMatch: 'full' },
 ];
---
+++ src/app/bazaar/bazaar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bazaar',
+  standalone: true,
+  imports: [],
+  templateUrl: './bazaar.component.html',
+  styleUrl: './bazaar.component.scss'
+})
+export class BazaarComponent {
+
+}
